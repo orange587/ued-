71bfc77c18833b19c721c2dd57ea269fe2d2bb59
huazai587 <huaanghua@163.com>
2017-12-14 21:46:54 +0800

    fisst

--- vue-ued/src/page/list_banner.vue
+++ vue-ued/src/page/list_banner.vue
@@ -7,25 +7,14 @@
         <!--选择头 st-->
         <div class="choice_wrap">
             <ul class="choice_xuan">
-                <li>
-                    <a href="" class="active">全部</a><a href="">2017</a><a href="">2016</a><a href="">2015</a><a href="">2014</a><a href="">2013</a><a href="">2012</a><a href="">2011</a>
+                     <li>
+                    <a  v-for="(num,index) in years" @click="iscur = index,worksAll(index)"  :class="{active:iscur==index}" :key="num.index" >{{num.titleTab }}</a>
                 </li>
                 <li>
-                    <a href="">全部</a><a href="" class="active">新房DL</a><a href="">新房AI</a><a href="">平台运营</a><a href="">二媒</a><a href="">电商</a><a href="">市场部</a>
+                    <a  v-for="(num,index) in departs" @click="iscur1 = index,worksAllDep(index)"  :class="{active:iscur1==index}" :key="num.index" >{{num.titleTab }}</a>
                 </li>
                 <li class="sexi">
-                    <a href="" class="all_color">全部</a>
-                    <a href="" class="sexi01 active"></a>
-                    <a href="" class="sexi02"></a>
-                    <a href="" class="sexi03"></a>
-                    <a href="" class="sexi04"></a>
-                    <a href="" class="sexi05"></a>
-                    <a href="" class="sexi06"></a>
-                    <a href="" class="sexi07"></a>
-                    <a href="" class="sexi08"></a>
-                    <a href="" class="sexi09"></a>
-                    <a href="" class="sexi10"></a>
-                    <a href="" class="sexi11"></a>
+                    <a  v-for="(num,index) in colors" @click="iscur2 = index,worksAllColor(index)"  :class="{active:iscur2==index}" :key="num.index" >{{num.titleTab }}</a>
                 </li>
             </ul>
 
@@ -104,7 +93,43 @@ export default {
   },
   data () {
         return {
-          PcLists: []
+          PcLists: [],
+           years:[
+              {titleTab:'全部'},
+              {titleTab:'2016'},
+              {titleTab:'2017'},
+              {titleTab:'2018'}
+          ],
+          departs:[
+               {titleTab:'全部'},
+              {titleTab:'新房DL'},
+              {titleTab:'新房AI'},
+              {titleTab:'平台运营'},
+              {titleTab:'二媒'},
+              {titleTab:'二电'},
+              {titleTab:'市场部'},
+              {titleTab:'其它部门'}
+          ],
+          colors:[
+               {titleTab:'全部'},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+               {titleTab:''},
+          ],
+          iscur:0 ,
+          iscur1:0 ,
+          iscur2:0 ,
+          typeIndex:0,
+          typesIndex:0,
+          index:0
         }
       },
        mounted () {
@@ -120,6 +145,45 @@ export default {
                   console.log(e)
                 })
         },
+          // 年份切换展示
+        worksAll (index) {
+          let yearstime = [0,2016,2017,2018];
+          this.yearIndex = yearstime[index];
+          this.$http.get(`${this.$url}?c=index&a=showBannerList&selYear=${this.yearIndex}&page=current&from=index&pagesize=25`)
+          .then((res) => {
+            this.stories = res.data.errmsg;
+            })
+            .catch(e => {
+                  console.log(e)
+            });
+        },
+       // 部门切换展示
+        worksAllDep (index) {
+          let yearstime = [0,2016,2017,2018];
+          this.yearIndex = yearstime[index];
+          this.depIndex = index;
+          this.$http.get(`${this.$url}?c=index&a=showBannerList&selYear=${this.yearIndex}&ownGroup=${this.depIndex}&page=current&from=index&pagesize=25`)
+          .then((res) => {
+            this.stories = res.data.errmsg;
+            })
+            .catch(e => {
+                  console.log(e)
+            });
+        },
+         // 颜色切换展示
+        worksAllColor (index) {
+          let yearstime = [0,2016,2017,2018];
+          this.yearIndex = yearstime[index];
+          this.depIndex = index;
+          this.$http.get(`${this.$url}?c=index&a=showBannerList&selYear=${this.yearIndex}&ownGroup=${this.depIndex}&page=current&from=index&pagesize=25`)
+          .then((res) => {
+            this.stories = res.data.errmsg;
+            })
+            .catch(e => {
+                  console.log(e)
+            });
+        },
+        
         }
 }
 </script>
--- vue-ued/src/page/list_pc.vue
+++ vue-ued/src/page/list_pc.vue
@@ -26,7 +26,7 @@
                     <a href="" class="sexi09"></a>
                     <a href="" class="sexi10"></a>
                     <a href="" class="sexi11"></a> -->
-                    <a  v-for="(num,index) in colors" @click="iscur2 = index,worksAllColor(index)"  :class="{active:iscur1==index}" :key="num.index" >{{num.titleTab }}</a>
+                    <a  v-for="(num,index) in colors" @click="iscur2 = index,worksAllColor(index)"  :class="{active:iscur2==index}" :key="num.index" >{{num.titleTab }}</a>
 
                 </li>
             </ul>
@@ -111,6 +111,7 @@ export default {
           ],
           iscur:0 ,
           iscur1:0 ,
+          iscur2:0 ,
           typeIndex:0,
           typesIndex:0,
           index:0
@@ -155,7 +156,19 @@ export default {
                   console.log(e)
             });
         },
-
+         // 颜色切换展示
+        worksAllColor (index) {
+          let yearstime = [0,2016,2017,2018];
+          this.yearIndex = yearstime[index];
+          this.depIndex = index;
+          this.$http.get(`${this.$url}?c=index&a=showPcList&selYear=${this.yearIndex}&ownGroup=${this.depIndex}&page=current&from=index&pagesize=25`)
+          .then((res) => {
+            this.stories = res.data.errmsg;
+            })
+            .catch(e => {
+                  console.log(e)
+            });
+        },
         
         }
 }
